599913b962e7e0859be2fdf55bb908896e746d97
Guilherme <guinovembro43@gmail.com>
2024-10-18 00:18:21 -0300

    Adding translation to component

--- components/contact/Contact.vue
+++ components/contact/Contact.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useContactStore } from "~/stores/useContactStore";
+import { useI18n } from "vue-i18n";
 
+const { t } = useI18n();
 const contactStore = useContactStore();
 const email = computed({
   get() {
@@ -28,21 +30,25 @@ const message = computed({
 });
 
 const emailRules = [
-  (value) => !!value || "Email é obrigatório.",
+  (value) => !!value || t("RequiredField", { field: t("Email") }),
   (value) =>
     /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-    "Digite um email válido.",
+    t("InvalidField", { field: t("Email") }),
 ];
-const nameRules = [(value) => !!value || "Nome é obrigatório."];
-const messageRules = [(value) => !!value || "Mensagem é obrigatória."];
+const nameRules = [
+  (value) => !!value || t("RequiredField", { field: t("Name") }),
+];
+const messageRules = [
+  (value) => !!value || t("RequiredField", { field: t("Message") }),
+];
 
 const isFormValid = ref(false);
 </script>
 <template>
   <section id="contact">
     <div class="title text-center pa-2">
-      <h2>Fale comigo hoje mesmo!</h2>
-      <p>Entre em contato via este formulário</p>
+      <h2>{{ $t("ContactMeToday") }}</h2>
+      <p>{{ $t("ContactMeForm") }}</p>
     </div>
     <v-row class="pa-0 ma-0">
       <v-col cols="1" sm="3" md="4" lg="4"></v-col>
@@ -53,28 +59,28 @@ const isFormValid = ref(false);
               v-model="name"
               :rules="nameRules"
               class="mb-2"
-              label="Nome"
-              placeholder="Digite seu nome"
+              :label="$t('Name')"
+              :placeholder="$t('WriteYourName')"
             >
             </v-text-field>
             <v-text-field
               v-model="email"
               :rules="emailRules"
               class="mb-2"
-              label="Email"
-              placeholder="Digite seu email"
+              :label="$t('Email')"
+              :placeholder="$t('WriteYourEmail')"
             >
             </v-text-field>
             <v-textarea
               v-model="message"
               :rules="messageRules"
               class="mb-2"
-              label="Mensagem"
-              placeholder="Escreva uma mensagem"
+              :label="$t('Message')"
+              :placeholder="$t('WriteAMessage')"
             >
             </v-textarea>
 
-            <v-btn color="success" block> Entrar em contato </v-btn>
+            <v-btn color="success" block> {{ $t("SendMessage") }} </v-btn>
           </v-form>
         </v-card>
       </v-col>
